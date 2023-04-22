5858c10886800c37b769120a7dff201fd223c274
BuildTools <unconfigured@null.spigotmc.org>
2023-04-22 14:24:18 -0400

    final

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,13 +30,19 @@
         {
             plus = new Button();
             returnbox = new TextBox();
+            add1 = new TextBox();
+            panel1 = new Panel();
+            clear = new Button();
+            output = new Label();
+            add2 = new TextBox();
+            panel1.SuspendLayout();
             SuspendLayout();
             // 
             // plus
             // 
-            plus.Location = new Point(98, 171);
+            plus.Location = new Point(165, 3);
             plus.Name = "plus";
-            plus.Size = new Size(25, 25);
+            plus.Size = new Size(25, 23);
             plus.TabIndex = 0;
             plus.Text = "+";
             plus.UseVisualStyleBackColor = true;
@@ -52,16 +58,69 @@
             returnbox.TabIndex = 1;
             returnbox.TextChanged += textBox1_TextChanged;
             // 
+            // add1
+            // 
+            add1.Location = new Point(3, 3);
+            add1.Name = "add1";
+            add1.PlaceholderText = "number 1";
+            add1.Size = new Size(100, 23);
+            add1.TabIndex = 2;
+            add1.TextChanged += textBox1_TextChanged_1;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(clear);
+            panel1.Controls.Add(output);
+            panel1.Controls.Add(add2);
+            panel1.Controls.Add(add1);
+            panel1.Controls.Add(plus);
+            panel1.Location = new Point(12, 12);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(360, 237);
+            panel1.TabIndex = 3;
+            // 
+            // clear
+            // 
+            clear.Location = new Point(140, 140);
+            clear.Name = "clear";
+            clear.Size = new Size(75, 23);
+            clear.TabIndex = 5;
+            clear.Text = "Clear";
+            clear.UseVisualStyleBackColor = true;
+            clear.Click += clear_Click;
+            // 
+            // output
+            // 
+            output.AutoSize = true;
+            output.Location = new Point(150, 100);
+            output.Name = "output";
+            output.Size = new Size(54, 15);
+            output.TabIndex = 4;
+            output.Text = "= output";
+            output.TextAlign = ContentAlignment.MiddleCenter;
+            output.Click += label1_Click;
+            // 
+            // add2
+            // 
+            add2.Location = new Point(257, 3);
+            add2.Name = "add2";
+            add2.PlaceholderText = "number 2";
+            add2.Size = new Size(100, 23);
+            add2.TabIndex = 3;
+            add2.TextChanged += textBox2_TextChanged;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(384, 361);
+            ClientSize = new Size(384, 261);
+            Controls.Add(panel1);
             Controls.Add(returnbox);
-            Controls.Add(plus);
             Name = "Form1";
             Text = "Calc";
             Load += Form1_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -70,5 +129,10 @@
 
         private Button plus;
         private TextBox returnbox;
+        private TextBox add1;
+        private Panel panel1;
+        private TextBox add2;
+        private Label output;
+        private Button clear;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,7 +1,12 @@
+using System.Runtime.InteropServices;
+
 namespace Lab_13
 {
     public partial class Form1 : Form
     {
+        double num1;
+        double num2;
+
         public Form1()
         {
             InitializeComponent();
@@ -9,7 +14,7 @@ namespace Lab_13
 
         private void plus_Click(object sender, EventArgs e)
         {
-
+            output.Text = ("= " + (num1 + num2));
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
@@ -19,7 +24,48 @@ namespace Lab_13
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            //AllocConsole();
+        }
+        //[DllImport("kernel32.dll", SetLastError = true)]
+        //[return: MarshalAs(UnmanagedType.Bool)]
+        //static extern bool AllocConsole();
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+            if (double.TryParse(add1.Text, out _))
+            {
+                num1 = double.Parse(add1.Text);
+            }
+            else
+            {
+                Console.WriteLine(add1.Text);
+                num1 = 0;
+            }
+        }
 
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (double.TryParse(add2.Text, out _))
+            {
+                num2 = double.Parse(add2.Text);
+            }
+            else
+            {
+                num2 = 0;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void clear_Click(object sender, EventArgs e)
+        {
+            num1 = 0;
+            num2 = 0;
+            output.Text = "= output";
+            add1.Text = "";
+            add2.Text = "";
         }
     }
 }
